f8d1c7360b19bafe4256c7eb7eb809942b3c1195
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-05-19 06:58:57 +0100

    updated the production parameter

--- Backend/src/sequelize/config/config.js
+++ Backend/src/sequelize/config/config.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
-console.log(process.env.DATABASE_URL);
+
 module.exports = {
   development: {
-    url: "postgres://novo_cryptoalert_user:W547inxp3dNl8UDldHfIv5Vg5kZe99en@dpg-ch9o14l269v0obbp2ltg-a.oregon-postgres.render.com/novo_cryptoalert",
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
-        require: true,
+        require: false,
       },
     },
   },
@@ -15,7 +15,12 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    host: "127.0.0.1",
-    dialect: 'mysql',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: false,
+      },
+    },
   },
 };
