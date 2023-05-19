0950292e9fc32b414d1e8c0a2c0e34fb34b85015
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-05-19 06:14:15 +0100

    hardcode

--- Backend/src/sequelize/config/config.js
+++ Backend/src/sequelize/config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    url: process.env.DATABASE_URL,
+    url: "postgres://novo_cryptoalert_user:W547inxp3dNl8UDldHfIv5Vg5kZe99en@dpg-ch9o14l269v0obbp2ltg-a.oregon-postgres.render.com/novo_cryptoalert",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
